7c33e86ea51cfe9eeb24aed5f5bfe2f26c46a7e8
guiac <gui.acassemiro@gmail.com>
2023-01-09 07:51:44 -0300

    :sparkles: Should call AddTenant with correct values

--- src/presentation/controllers/create.tenant.controller.ts
+++ src/presentation/controllers/create.tenant.controller.ts
@@ -1,15 +1,18 @@
+import { AddTenant } from '@/domain/usecases'
 import { Controller } from '@/presentation/protocols/controller'
 import { badRequest } from '../helpers'
 import { Validation } from '../protocols/validation'
 
 export class CreateTenantController implements Controller {
     constructor(
-        private readonly validation: Validation
+        private readonly validation: Validation,
+        private readonly addTenant: AddTenant
     ) { }
 
     async handle(data: CreateTenantController.Request): Promise<any> {
         const validationError = this.validation.validate(data)
         if (validationError) return badRequest(validationError)
+        await this.addTenant.handle(data)
     }
 }
 
