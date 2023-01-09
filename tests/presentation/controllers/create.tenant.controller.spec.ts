73bd8695a3301ce6f1713308f00d7266a665c36d
guiac <gui.acassemiro@gmail.com>
2023-01-08 22:34:44 -0300

    :test_tube: Should call validation with correct values

---
+++ tests/presentation/controllers/create.tenant.controller.spec.ts
@@ -0,0 +1,39 @@
+import { CreateTenantController } from '@/presentation/controllers'
+import { ValidationSpy } from '../mocks'
+
+const mockRequest = (): CreateTenantController.Request => ({
+    tenantId: 'valid_tenantId',
+    companyName: 'valid_companyName',
+    companyEmail: 'valid_companyEmail',
+    companyTellphone: 'valid_companyTellphone',
+    companyCellphone: 'valid_companyCellphone',
+    companyCnpj: 'valid_companyCnpj',
+    companyAddress: 'valid_companyAddress',
+    companyState: 'valid_companyState',
+    companyNumber: 'valid_companyNumber',
+    companyDistrict: 'valid_companyDistrict',
+    companyCity: 'valid_companyCity',
+})
+
+type SutTypes = {
+    sut: CreateTenantController
+    validationSpy: ValidationSpy
+}
+
+const makeSut = (): SutTypes => {
+    const validationSpy = new ValidationSpy()
+    const sut = new CreateTenantController(validationSpy)
+    return {
+        validationSpy,
+        sut
+    }
+}
+
+describe('CreateTenantController', () => {
+    test('Should call validation with correct values', async () => {
+        const { sut, validationSpy } = makeSut()
+        const request = mockRequest()
+        await sut.handle(request)
+        expect(validationSpy.input).toEqual(request)
+    })
+})
