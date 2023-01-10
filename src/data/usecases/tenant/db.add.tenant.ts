42d9bf0e6940d742087629061629619fcf5eed54
guiac <gui.acassemiro@gmail.com>
2023-01-09 23:09:19 -0300

    :sparkles: Should throw if CreateUuid throws

--- src/data/usecases/tenant/db.add.tenant.ts
+++ src/data/usecases/tenant/db.add.tenant.ts
@@ -1,13 +1,16 @@
 import { AddTenant } from '@/domain/usecases'
 import { CheckTenantByEmailRepository } from '@/data/protocols/db'
+import { CreateUuid } from '@/data/protocols'
 
 export class DbAddTenant implements AddTenant {
     constructor(
-        private readonly checkTenantByEmailRepository: CheckTenantByEmailRepository
+        private readonly checkTenantByEmailRepository: CheckTenantByEmailRepository,
+        private readonly createUuid: CreateUuid
     ) { }
 
     async handle(data: AddTenant.Request): Promise<any> {
         const company = await this.checkTenantByEmailRepository.check(data.companyEmail)
         if (company) return !company
+        this.createUuid.create()
     }
 }
