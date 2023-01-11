aadd1281c6660aa2d84ae329b8718d52578aec01
guiac <gui.acassemiro@gmail.com>
2023-01-10 22:07:45 -0300

    :sparkles: Should return company if email is valid

--- src/infra/db/mongodb/tenant.mongo.repository.ts
+++ src/infra/db/mongodb/tenant.mongo.repository.ts
@@ -1,10 +1,14 @@
-import { AddTenantRepository } from '@/data/protocols'
+import { AddTenantRepository, CheckTenantByEmailRepository } from '@/data/protocols'
 import { TenantModel } from './models'
 
-export class TenantMongoRepository implements AddTenantRepository {
+export class TenantMongoRepository implements AddTenantRepository, CheckTenantByEmailRepository {
     async save(data: AddTenantRepository.Params): Promise<AddTenantRepository.Result> {
         const model = new TenantModel(data)
         const result = await model.save()
         return result
+    }
+
+    async check(email: string): Promise<CheckTenantByEmailRepository.Result> {
+        return await TenantModel.findOne({ email }).lean()
     }
 }
