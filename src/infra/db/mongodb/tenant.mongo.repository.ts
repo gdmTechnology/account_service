a090b30088b869f01b9cab64affeb63b86672f7e
guiac <gui.acassemiro@gmail.com>
2023-01-11 23:44:08 -0300

    :sparkles: Should return company if tenant is valid

--- src/infra/db/mongodb/tenant.mongo.repository.ts
+++ src/infra/db/mongodb/tenant.mongo.repository.ts
@@ -1,7 +1,7 @@
-import { AddTenantRepository, CheckTenantByEmailRepository } from '@/data/protocols'
+import { AddTenantRepository, CheckTenantByEmailRepository, LoadTenantRepository } from '@/data/protocols'
 import { TenantModel } from './models'
 
-export class TenantMongoRepository implements AddTenantRepository, CheckTenantByEmailRepository {
+export class TenantMongoRepository implements AddTenantRepository, CheckTenantByEmailRepository, LoadTenantRepository {
     async save(data: AddTenantRepository.Params): Promise<AddTenantRepository.Result> {
         const model = new TenantModel(data)
         const result = await model.save()
@@ -10,5 +10,9 @@ export class TenantMongoRepository implements AddTenantRepository, CheckTenantBy
 
     async check(email: string): Promise<CheckTenantByEmailRepository.Result> {
         return await TenantModel.findOne({ email }).lean()
+    }
+
+    async load(tenant: string): Promise<LoadTenantRepository.Result> {
+        return await TenantModel.findOne({ tenant }).lean()
     }
 }
