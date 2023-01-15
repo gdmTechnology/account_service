69515ce10718590e6c1fc6aa50486feec79416cc
guiac <gui.acassemiro@gmail.com>
2023-01-15 16:50:35 -0300

    :sparkles: Should call CheckAccountByIdRepository with correct values

--- src/data/usecases/account/db.create.admin.ts
+++ src/data/usecases/account/db.create.admin.ts
@@ -1,6 +1,6 @@
 import { CreateAdmin } from '@/domain/usecases'
 import { Hasher, CreateUuid } from '@/data/protocols/cryptography'
-import { AddAccountRepository, CheckAccountByEmailRepository } from '@/data/protocols/db'
+import { AddAccountRepository, CheckAccountByEmailRepository, CheckAccountByIdRepository } from '@/data/protocols/db'
 import { Constants } from '@/helper'
 
 export class DbCreateAdmin implements CreateAdmin {
@@ -8,16 +8,20 @@ export class DbCreateAdmin implements CreateAdmin {
         private readonly createUuid: CreateUuid,
         private readonly hasher: Hasher,
         private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository,
+        private readonly checkAccountByIdRepository: CheckAccountByIdRepository,
         private readonly addAccountRepository: AddAccountRepository
     ) { }
 
-    async handle(data: CreateAdmin.Request): Promise<CreateAdmin.Result> {
+    async handle(data: CreateAdmin.Request, adminId: string): Promise<CreateAdmin.Result> {
         const existsAcc = await this.checkAccountByEmailRepository.checkAccountByEmail(data.email)
         if (existsAcc) return Constants.EmailInUseError
 
+        const checkAdmin = await this.checkAccountByIdRepository.checkAccountById(adminId)
+        if (checkAdmin) return Constants.Forbidden
+
         const hashedPassword = await this.hasher.hash(data.password)
         const identification = this.createUuid.create()
-        const account = await this.addAccountRepository.save({ ...data, tenant: '', identification, password: hashedPassword })
-        return account
+        const admin = await this.addAccountRepository.save({ ...data, identification, password: hashedPassword })
+        return admin
     }
 }
