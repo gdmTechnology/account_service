641b83c7d7f800854806bddd0a9c2e3a85e83877
guiac <gui.acassemiro@gmail.com>
2023-01-15 14:25:28 -0300

    :recycle: refactor account role

--- src/presentation/controllers/account/signup.controller.ts
+++ src/presentation/controllers/account/signup.controller.ts
@@ -19,7 +19,7 @@ export class SignUpController implements Controller {
                 return badRequest(error)
             }
             const { passwordConfirmation, ...newAccount } = data
-            const isValid = await this.addAccount.handle({ ...newAccount, role: 'user' })
+            const isValid = await this.addAccount.handle({ ...newAccount, role: null })
 
             if (isValid === Constants.EmailInUseError) {
                 return forbidden(new EmailInUseError())
