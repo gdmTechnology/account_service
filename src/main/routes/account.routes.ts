934e8209701ae046aef1f8609099d068832a6fd8
guiac <gui.acassemiro@gmail.com>
2023-01-15 18:07:41 -0300

    :sparkles: create admin route

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -1,11 +1,12 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController } from '@/main/factories'
+import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController, makeCreateAdminController } from '@/main/factories'
 
 import { Router } from 'express'
-import { auth } from '@/main/middlewares'
+import { auth, authAdmin } from '@/main/middlewares'
 
 export default (router: Router): void => {
     router.post('/signup', adaptRoute(makeSignUpController()))
     router.post('/login', adaptRoute(makeAuthenticationController()))
     router.put('/account/update/:identification', auth, adaptRoute(makeUpdateAccountControler()))
+    router.post('/account/admin', authAdmin, adaptRoute(makeCreateAdminController()))
 }
