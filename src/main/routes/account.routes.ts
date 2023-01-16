5e810f4c330ce073787f6abed3c534709c3fc048
guiac <gui.acassemiro@gmail.com>
2023-01-16 12:57:44 -0300

    :sparkles: List users route

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -1,5 +1,5 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController, makeCreateAdminController } from '@/main/factories'
+import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController, makeCreateAdminController, makeListUsersController } from '@/main/factories'
 
 import { Router } from 'express'
 import { auth, authAdmin } from '@/main/middlewares'
@@ -8,5 +8,6 @@ export default (router: Router): void => {
     router.post('/signup', adaptRoute(makeSignUpController()))
     router.post('/login', adaptRoute(makeAuthenticationController()))
     router.put('/account/update/:identification', auth, adaptRoute(makeUpdateAccountControler()))
+    router.get('/account', authAdmin, adaptRoute(makeListUsersController()))
     router.post('/account/admin', authAdmin, adaptRoute(makeCreateAdminController()))
 }
