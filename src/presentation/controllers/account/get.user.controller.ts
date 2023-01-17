259122f7d800e187ff49dc04eef3912f9d85a993
guiac <gui.acassemiro@gmail.com>
2023-01-17 20:49:47 -0300

    :sparkles: Should call Validation with correct values

--- src/presentation/controllers/account/get.user.controller.ts
+++ src/presentation/controllers/account/get.user.controller.ts
@@ -1,14 +1,17 @@
 import { ListUsers, GetUser } from '@/domain/usecases'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 import { ok, serverError } from '@/presentation/helpers'
+import { Validation } from '@/presentation/protocols/validation'
 
 export class GetUserController implements Controller {
     constructor(
+        private readonly validation: Validation,
         private readonly getUser: GetUser
     ) { }
 
     async handle(data: GetUserController.Request): Promise<any> {
         // try {
+        this.validation.validate(data)
         const user = await this.getUser.handle(data)
         //     return ok(users)
         // } catch (error) {
