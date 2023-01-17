d5ca8a5fd85ef618abe0e2228aecb86f792dda6e
guiac <gui.acassemiro@gmail.com>
2023-01-17 07:49:44 -0300

    :sparkles: Delete account route

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -1,5 +1,5 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController, makeCreateAdminController, makeListUsersController } from '@/main/factories'
+import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController, makeCreateAdminController, makeListUsersController, makeDeleteAccountController } from '@/main/factories'
 
 import { Router } from 'express'
 import { auth, authAdmin } from '@/main/middlewares'
@@ -7,7 +7,8 @@ import { auth, authAdmin } from '@/main/middlewares'
 export default (router: Router): void => {
     router.post('/signup', adaptRoute(makeSignUpController()))
     router.post('/login', adaptRoute(makeAuthenticationController()))
-    router.put('/account/update/:identification', auth, adaptRoute(makeUpdateAccountControler()))
+    router.put('/account/:identification', auth, adaptRoute(makeUpdateAccountControler()))
     router.get('/account', authAdmin, adaptRoute(makeListUsersController()))
+    router.delete('/account/:identification', authAdmin, adaptRoute(makeDeleteAccountController()))
     router.post('/account/admin', authAdmin, adaptRoute(makeCreateAdminController()))
 }
