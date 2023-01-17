4a4e6295b7e3d0d1cee0053dd0622125c9e8384f
guiac <gui.acassemiro@gmail.com>
2023-01-16 21:37:18 -0300

    :sparkles: Should call DeleteAccountRepository with correct values

--- src/data/usecases/account/db.delete.account.ts
+++ src/data/usecases/account/db.delete.account.ts
@@ -1,15 +1,15 @@
 import { DeleteAccount } from '@/domain/usecases'
 import { LoadAccountByIdRepository, DeleteAccountRepository } from '@/data/protocols/db'
-import { Constants } from '@/helper'
 
 export class DbDeleteAccount implements DeleteAccount {
     constructor(
-        private readonly loadAccountByIdRepository: LoadAccountByIdRepository
-        // private readonly addAccountRepository: LoadAccountByIdRepository
+        private readonly loadAccountByIdRepository: LoadAccountByIdRepository,
+        private readonly deleteAccountRepository: DeleteAccountRepository
     ) { }
 
     async handle(identification: string): Promise<any> {
         const account = await this.loadAccountByIdRepository.loadAccountById(identification)
         if (!account) return !!account
+        await this.deleteAccountRepository.delete(identification)
     }
 }
