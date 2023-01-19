f7abf529a81f637540630bf9d9c0d8ee9dc6925f
guiac <gui.acassemiro@gmail.com>
2023-01-18 22:14:52 -0300

    :sparkles: get user route

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -1,5 +1,5 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController, makeCreateAdminController, makeListUsersController, makeDeleteAccountController } from '@/main/factories'
+import { makeSignUpController, makeUpdateAccountControler, makeAuthenticationController, makeCreateAdminController, makeListUsersController, makeDeleteAccountController, makeGetUserController } from '@/main/factories'
 
 import { Router } from 'express'
 import { auth, authAdmin } from '@/main/middlewares'
@@ -9,6 +9,7 @@ export default (router: Router): void => {
     router.post('/login', adaptRoute(makeAuthenticationController()))
     router.put('/account/:identification', auth, adaptRoute(makeUpdateAccountControler()))
     router.get('/account', authAdmin, adaptRoute(makeListUsersController()))
+    router.get('/account/:identification', authAdmin, adaptRoute(makeGetUserController()))
     router.delete('/account/:identification', authAdmin, adaptRoute(makeDeleteAccountController()))
     router.post('/account/admin', authAdmin, adaptRoute(makeCreateAdminController()))
 }
